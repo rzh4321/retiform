9bee8a469f00eb80abed4727ba3644bb4a095b54
Ricky <rzh4321@gmail.com>
2023-10-06 16:26:41 -0400

    users cant access api routes from url search bar

---
+++ middleware.js
@@ -0,0 +1,26 @@
+import { NextResponse } from 'next/server'
+import { headers } from 'next/headers';
+import url from 'url';
+
+ 
+export async function middleware(request) {
+    const headersList = headers()
+    const referer = headersList.get('referer');
+    console.log(referer)
+    if (referer === null) {
+      console.log('referer is null')
+      return NextResponse.json('no');
+    }
+    const parsedUrl = url.parse(referer);
+    const baseUrl = `${parsedUrl.protocol}//${parsedUrl.host}`;
+    console.log('requesting resource ', request.url, ' ITS FROM ', baseUrl);
+    if (baseUrl === 'http://localhost:3000' || baseUrl === 'https://retiform.vercel.app') {
+      return NextResponse.next();
+    }
+    return NextResponse.json('no');
+}
+ 
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: '/api/:path*',
+}
